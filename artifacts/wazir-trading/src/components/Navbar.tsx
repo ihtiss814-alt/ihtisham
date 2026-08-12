6b43731913cbc44cbb04cbc3b376125be907f2f9
ihtiss3636-afk <ihtiss3636@gmail.com>
2026-08-12 15:49:38 +0500

    Changes to be committed: #	modified:   artifacts/wazir-trading/src/components/Navbar.tsx #

--- artifacts/wazir-trading/src/components/Navbar.tsx
+++ artifacts/wazir-trading/src/components/Navbar.tsx
@@ -212,7 +212,7 @@ export default function Navbar() {
                   <img
                     src="/logo-small.png"
                     alt="Wazir Trading LLC"
-                    className="h-[48px] md:h-[76px] w-auto transition-opacity duration-200 group-hover:opacity-85"
+                    className="h-[56px] md:h-[64px] w-auto transition-opacity duration-200 group-hover:opacity-85"
                     style={{ mixBlendMode: 'multiply' }}
                     loading="eager"
                     decoding="sync"
@@ -371,7 +371,7 @@ export default function Navbar() {
               <img
                 src="/logo-small.png"
                 alt="Wazir Trading LLC"
-                className="h-[48px] w-auto"
+                className="h-[56px] w-auto"
                 style={{ mixBlendMode: 'multiply' }}
                 loading="eager"
                 decoding="sync"
